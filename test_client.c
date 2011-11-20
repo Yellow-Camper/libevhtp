b53f25f7e7df6af8512569c3c07b11a5856b23a3
Mark Ellzey <mark.thomas@mandiant.com>
2011-11-20 17:17:04 -0500

    updating test_client.c

--- test_client.c
+++ test_client.c
@@ -8,8 +8,18 @@
 static void
 request_cb(evhtp_request_t * req, void * arg) {
     printf("hi %zu\n", evbuffer_get_length(req->buffer_in));
-    printf("%.*s\n", evbuffer_get_length(req->buffer_in), 
-	    evbuffer_pullup(req->buffer_in, -1));
+    printf("%.*s\n", evbuffer_get_length(req->buffer_in),
+           evbuffer_pullup(req->buffer_in, -1));
+
+    event_base_loopbreak((evbase_t *)arg);
+}
+
+static evhtp_res
+print_data(evhtp_request_t * req, evbuf_t * buf, void * arg) {
+    printf("Got %zu bytes\n",
+           evbuffer_get_length(buf));
+
+    return EVHTP_RES_OK;
 }
 
 int
@@ -20,7 +30,9 @@ main(int argc, char ** argv) {
 
     evbase  = event_base_new();
     conn    = evhtp_connection_new(evbase, "75.126.169.52", 80);
-    request = evhtp_request_new(request_cb, NULL);
+    request = evhtp_request_new(request_cb, evbase);
+
+    evhtp_set_hook(&request->hooks, evhtp_hook_on_read, print_data, evbase);
 
     evhtp_headers_add_header(request->headers_out,
                              evhtp_header_new("Host", "ieatfood.net", 0, 0));

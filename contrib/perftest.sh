c06ef72bf47fd077b53922f0f4b3e9f4b95d0358
Mark Ellzey <mark.thomas@mandiant.com>
2011-12-12 15:17:25 -0500

    Added a basic example

--- contrib/perftest.sh
+++ contrib/perftest.sh
@@ -14,6 +14,6 @@ calls=$5
 
 echo "Running ($conns connections | $calls requests each)"
 
-httperf --ssl --server=$host --port=$port --uri=$uri --rate=1e+06 --send-buffer=4096 --recv-buffer=16384 --wsess=$conns,$calls,0
+#httperf --ssl --server=$host --port=$port --uri=$uri --rate=1e+06 --send-buffer=4096 --recv-buffer=16384 --wsess=$conns,$calls,0
 #httperf --print-reply header --add-header "Accept-Encoding: gzip,deflate,sdch\n" --server=$host --port=$port --uri=$uri --rate=1e+06 --send-buffer=4096 --recv-buffer=16384 --wsess=$conns,$calls,0
-#httperf --ssl-no-reuse --ssl --server=$host --port=$port --uri=$uri --rate=1e+06 --send-buffer=4096 --recv-buffer=16384 --wsess=$conns,$calls,0
+httperf --server=$host --port=$port --uri=$uri --rate=1e+06 --send-buffer=4096 --recv-buffer=16384 --wsess=$conns,$calls,0

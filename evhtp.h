a3731d70d40de4127c0d3825a812397a95e86310
Mark Ellzey <mark.thomas@mandiant.com>
2011-12-28 15:04:32 -0500

    Prepping release v0.4.5

--- evhtp.h
+++ evhtp.h
@@ -145,10 +145,10 @@ typedef void (*evhtp_ssl_scache_del)(evhtp_t * htp, unsigned char * sid, int sid
 typedef evhtp_ssl_sess_t * (*evhtp_ssl_scache_get)(evhtp_connection_t * connection, unsigned char * sid, int sid_len);
 typedef void * (*evhtp_ssl_scache_init)(evhtp_t *);
 
-#define EVHTP_VERSION          "0.4.4"
+#define EVHTP_VERSION          "0.4.5"
 #define EVHTP_VERSION_MAJOR    0
 #define EVHTP_VERSION_MINOR    4
-#define EVHTP_VERSION_PATCH    4
+#define EVHTP_VERSION_PATCH    5
 
 #define evhtp_headers_iterator evhtp_kvs_iterator
 

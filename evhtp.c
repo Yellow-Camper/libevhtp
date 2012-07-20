cc50f84e623249566f0f2b563e071515bb7c19a8
Mark Ellzey <mark.thomas@mandiant.com>
2012-07-20 14:24:44 -0400

    Drain before letting ownership change.

--- evhtp.c
+++ evhtp.c
@@ -1461,6 +1461,7 @@ _evhtp_connection_readcb(evbev_t * bev, void * arg) {
     nread = htparser_run(c->parser, &request_psets, (const char *)buf, avail);
 
     if (c->owner != 1) {
+	evbuffer_drain(bufferevent_get_input(bev), nred);
         return evhtp_connection_free(c);
     }
 

2b714c599a82d94d637428c9deb9882543e49b44
Janusz Dziemidowicz <rraptorr@nails.eu.org>
2013-08-02 13:02:17 +0200

    Use SSL_OP_NO_COMPRESSION if possible
    Use SSL_OP_NO_COMPRESSION if OpenSSL library supports it instead of
    clearing compression methods stack.

--- evhtp.c
+++ evhtp.c
@@ -3235,14 +3235,16 @@ evhtp_ssl_init(evhtp_t * htp, evhtp_ssl_cfg_t * cfg) {
     SSL_load_error_strings();
     RAND_poll();
 
+#if OPENSSL_VERSION_NUMBER < 0x10000000L
     STACK_OF(SSL_COMP) * comp_methods = SSL_COMP_get_compression_methods();
     sk_SSL_COMP_zero(comp_methods);
+#endif
 
     htp->ssl_cfg = cfg;
     htp->ssl_ctx = SSL_CTX_new(SSLv23_server_method());
 
 #if OPENSSL_VERSION_NUMBER >= 0x10000000L
-    SSL_CTX_set_options(htp->ssl_ctx, SSL_MODE_RELEASE_BUFFERS);
+    SSL_CTX_set_options(htp->ssl_ctx, SSL_MODE_RELEASE_BUFFERS | SSL_OP_NO_COMPRESSION);
     /* SSL_CTX_set_options(htp->ssl_ctx, SSL_MODE_AUTO_RETRY); */
     SSL_CTX_set_timeout(htp->ssl_ctx, cfg->ssl_ctx_timeout);
 #endif

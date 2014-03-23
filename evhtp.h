42995227f4990e0c84428104f19b24d4b11915df
Mark Ellzey <socket@gmail.com>
2014-03-23 07:50:35 -0400

    Release v1.2.9, see ChangeLog for details.

--- evhtp.h
+++ evhtp.h
@@ -169,10 +169,10 @@ typedef void (*evhtp_ssl_scache_del)(evhtp_t * htp, unsigned char * sid, int sid
 typedef evhtp_ssl_sess_t * (*evhtp_ssl_scache_get)(evhtp_connection_t * connection, unsigned char * sid, int sid_len);
 typedef void * (*evhtp_ssl_scache_init)(evhtp_t *);
 
-#define EVHTP_VERSION           "1.2.8"
+#define EVHTP_VERSION           "1.2.9"
 #define EVHTP_VERSION_MAJOR     1
 #define EVHTP_VERSION_MINOR     2
-#define EVHTP_VERSION_PATCH     8
+#define EVHTP_VERSION_PATCH     9
 
 #define evhtp_headers_iterator  evhtp_kvs_iterator
 

4cb782db77b0c2a1086250477ee66709b41828dc
Ultima1252 <ultima1252@gmail.com>
2018-01-11 08:07:12 -0800

    Update evhtp.c

--- evhtp.c
+++ evhtp.c
@@ -2373,14 +2373,14 @@ htp__connection_writecb_(struct bufferevent * bev, void * arg)
         return;
     }
 
-    /* run user-hook for on_write callback before further analysis */
-    htp__hook_connection_write_(conn);
-
     /* connection is in a paused state, no further processing yet */
     if ((conn->flags & EVHTP_CONN_FLAG_PAUSED))
     {
         return;
     }
+    
+    /* run user-hook for on_write callback before further analysis */
+    htp__hook_connection_write_(conn);
 
     if (conn->flags & EVHTP_CONN_FLAG_WAITING)
     {
@@ -3043,7 +3043,7 @@ evhtp_connection_pause(evhtp_connection_t * c)
 
     HTP_FLAG_ON(c, EVHTP_CONN_FLAG_PAUSED);
 
-    bufferevent_disable(c->bev, EV_READ | EV_WRITE);
+    bufferevent_disable(c->bev, EV_READ);
 
     return;
 }

8f3b3ecf7cf6158fa89cedf6a526445ad2626e6b
Nathan French <nate@cl0d.com>
2018-03-27 14:54:00 -0400

    Check the return of getsockname

--- examples/eutils.h
+++ examples/eutils.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 static void * mm__dup_(const void * src, size_t size) {
     void * mem = malloc(size);
 
@@ -12,14 +16,14 @@ static void * mm__dup_(const void * src, size_t size) {
 #define bind__sock_port0_(HTP) ({                      \
         struct sockaddr_in sin;                        \
         socklen_t len = len = sizeof(struct sockaddr); \
-        uint16_t port;                                 \
+        uint16_t port = 0;                             \
                                                        \
         evhtp_bind_socket(HTP, "127.0.0.1", 0, 128);   \
                                                        \
-        getsockname(                                   \
-            evconnlistener_get_fd(HTP->server),        \
-            (struct sockaddr *)&sin, &len);            \
-                                                       \
-        port = ntohs(sin.sin_port);                    \
+        if (getsockname(                               \
+                evconnlistener_get_fd(HTP->server),    \
+                (struct sockaddr *)&sin, &len) == 0) { \
+            port = ntohs(sin.sin_port);                \
+        }                                              \
         port;                                          \
     })
